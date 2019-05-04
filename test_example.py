949959583be4cfd76e5fe29d641cbf782019927d
mduldul <mertcanduldul@gmail.com>
2019-05-04 22:43:47 +0300

    README ADDED !

--- test_example.py
+++ test_example.py
@@ -19,7 +19,7 @@ class TestAmazonExample(WebDriverBase):
         super().__init__()
         self.init_driver()
         self.driver.get("https://www.amazon.com/ap/signin?_encoding=UTF8&ignoreAuthState=1&openid.assoc_handle=usflex&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fref%3Dnav_signin&switch_account=")
-        self.login("ENTER USERNAME","ENTER PASSWORD")
+        self.login("USERNAME","PASSWORD")
         self.test_category()
         self.test_search_product("samsung")
         self.product_click()
